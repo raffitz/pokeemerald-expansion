b4e8c1da2ed6e45767d12d3e18b29c64344b3478
Jaizu <jaizu85@gmail.com>
2020-12-06 12:52:04 +0100

    Walk with B if autorun is enabled

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -644,7 +644,11 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
     if (!(gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_UNDERWATER) && (heldKeys & B_BUTTON || gSaveBlock2Ptr->autoRun) && FlagGet(FLAG_SYS_B_DASH)
      && IsRunningDisallowed(gObjectEvents[gPlayerAvatar.objectEventId].currentMetatileBehavior) == 0)
     {
-        PlayerRun(direction);
+        if (heldKeys & B_BUTTON && gSaveBlock2Ptr->autoRun == TRUE)
+            PlayerGoSpeed1(direction);
+        else
+            PlayerRun(direction);
+        
         gPlayerAvatar.flags |= PLAYER_AVATAR_FLAG_DASH;
         return;
     }
